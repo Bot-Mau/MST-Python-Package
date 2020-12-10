bce475452970381081988b9921aed93cd4865e62
binlun98 <59858933+binlun98@users.noreply.github.com>
2020-12-10 00:03:17 -0600

    Changes min_cost_edge function

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 import networkx as nx
 
-#show set of vertercies
+#original graph
 def V(graph):
     return set(graph.nodes())
-#show set of edges
+#
 def E(graph):
     return set(graph.nodes())
 #creat graph
@@ -21,29 +21,19 @@ def is_spanning(graph, subgraph):
 
 #return weight of the edge
 def cost(G, e):
-    return G[e[0]][e[1]]['weight']
+    return G[e[0]][e[1]]["weight"]
 
 #merging/union edges that is duplicated
 def possible_edges(G, T):
-    incidentedges=[]
-    for e in G.edges():
-        for v in T.nodes():
-            if v in e:
-                incidentedges.append(e)
-            
-    validedges=[]                      
-    for e in incidentedges:
-        if e[0] not in T.nodes() \
-        or e[1] not in T.nodes():
-            validedges.append(e)            
-            
-    return validedges
-#sort() // temp_edge.sort() = (44:47)
+    return [e for e in list(G.edges(V(T)))
+            if e[0] not in V(T) or e[1] not in V(T)]
+
+
 #return lowest weight edge
 def min_cost_edge(G, T):
-    temp_edges = possible_edges(G, T)
+    temp_edges = possible_edges(G, T) #return possible edges
+    #comparing one edge to another by weight to get the smalles edge weight
     min_edge = temp_edges[0]
     for e in temp_edges:
-        if (cost(G, e) < cost(G, min_edge)):
-            min_edge=e
+        min_edge = e if  (cost(G, e) < cost(G, min_edge)) else min_edge
     return min_edge
