51ead6641a73aaf2f121c3a465f762675435c9e7
binlun98 <59858933+binlun98@users.noreply.github.com>
2020-12-10 00:00:18 -0600

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -1,5 +1,6 @@
+import networkx as nx
 from functions import *
-from drawing import *
+from drawing import show_weighted_graph, draw_subtree
 
 
 def prims_algorithm(G, starting_vertex, show_graph = False, show_cost = False):
@@ -19,6 +20,6 @@ def prims_algorithm(G, starting_vertex, show_graph = False, show_cost = False):
            draw_subtree(G, T)
    #show cost
    if show_cost == True:
-        c = sum[[cost(G, e) for e in E(T)]]
+        c = sum(cost(G, e) for e in T.edges())
         print(f'The cost of this spanning tree is {c}')
    return T
