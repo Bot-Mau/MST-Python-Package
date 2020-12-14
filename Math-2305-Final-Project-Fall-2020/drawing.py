07534ca3e192d52fb6141c552a57be0ded418509
Mauricio Aramburu <maramburu98@gmail.com>
2020-12-14 17:13:12 -0600

    Updated drawing.py

--- Math-2305-Final-Project-Fall-2020/drawing.py
+++ Math-2305-Final-Project-Fall-2020/drawing.py
@@ -1,19 +1,19 @@
-import networkx as nx
+import networkx as netx
 import matplotlib.pyplot as plt
 
 def show_weighted_graph(G):
-    pos = nx.planar_layout(G)
-    nx.draw(G, pos)
-    labels = nx.get_edge_attributes(G, 'weight')
-    nx.draw_networkx_edge_labels(G, pos, edge_labels= labels)
+    pos = netx.planar_layout(G)
+    netx.draw(G, pos)
+    labels = netx.get_edge_attributes(G, 'weight')
+    netx.draw_networkx_edge_labels(G, pos, edge_labels= labels)
     plt.show()
     
 def draw_subtree(G, T):
-    pos = nx.planar_layout(G)
-    nx.draw_networkx(G, pos)
-    labels = nx.get_edge_attributes(G, 'weight')
-    nx.draw_networkx(G, pos, with_labels=True)
-    nx.draw_networkx_edge_labels(G, pos, edge_labels= labels)
-    nx.draw_networkx_edges(G, pos, edgelist=T.edges(), width=0, alpha=0.5, edge_color='r',)
-    nx.draw_networkx_nodes(G, pos, nodelist=T.nodes(), node_color='r', node_size=500, alpha=0.8)
+    pos = netx.planar_layout(G)
+    netx.draw_networkx(G, pos)
+    labels = netx.get_edge_attributes(G, 'weight')
+    netx.draw_networkx(G, pos, with_labels=True)
+    netx.draw_networkx_edge_labels(G, pos, edge_labels= labels)
+    netx.draw_networkx_edges(G, pos, edgelist=T.edges(), width=0, alpha=0.5, edge_color='r',)
+    netx.draw_networkx_nodes(G, pos, nodelist=T.nodes(), node_color='r', node_size=500, alpha=0.8)
     plt.show()
