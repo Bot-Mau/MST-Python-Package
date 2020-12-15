19197df70fb37620ba30c17d36a986485a4e1504
binlun98 <59858933+binlun98@users.noreply.github.com>
2020-12-14 19:57:51 -0600

    added cmt

--- Math-2305-Final-Project-Fall-2020/algorithms.py
+++ Math-2305-Final-Project-Fall-2020/algorithms.py
@@ -4,12 +4,17 @@ from drawing import show_weighted_graph, draw_subtree
 
 
 def prims_algorithm(G, starting_vertex, show_graph = False, show_cost = False):
-   #draw graph
+   #The function reads the graph G and uses starting_vertex to run prim's algorithm
+   #it will create a temp graph T which is a copy of the main graph. 
+   #In the loop, T will be compare with G to check if T has already as big as G
+   #if T is not, then the function will add the minimum and possible edge from G to T.
+   #after the loop, the function will calculate all the edge in the graph if the user wish to see its cost
+   #the return value is the graph T
    if show_graph == True:
        show_weighted_graph(G)
-   #create graph
+   #create temp graph
    T = prims_init(G, starting_vertex)
-   #draw sub-graph
+   #draw T graph
    if show_graph == True:
        draw_subtree(G, T)
     
