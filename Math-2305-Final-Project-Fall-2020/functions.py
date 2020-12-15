2af9cd5b3f7498d9c2ff5f0a5c05acd1eb37696b
binlun98 <59858933+binlun98@users.noreply.github.com>
2020-12-14 20:18:25 -0600

    added cmts

--- Math-2305-Final-Project-Fall-2020/functions.py
+++ Math-2305-Final-Project-Fall-2020/functions.py
@@ -1,13 +1,16 @@
 import networkx as nx
 
-#original graph
 def V(graph):
+    #this function is used to return all nodes in the graph
     return set(graph.nodes())
-#
+
 def E(graph):
+    #this function is same with V(graph), but it is used to avoid same definition of function
+    #during comparision
     return set(graph.nodes())
-#create graph
+
 def prims_init(graph, v):
+    #This function adds node v into existing graph T. The return value is graph T after added node v
     if v not in V(graph):
         return "Enter vertex not found"
     else: 
@@ -15,23 +18,26 @@ def prims_init(graph, v):
         T.add_node(v)
         return T 
     
-#check if the tree is equal to its subgraph
 def is_spanning(graph, subgraph):
+    #this version compare 2 graph and return true if they are the same (size), otherwise, the fuction return false
     return V(graph) == V(subgraph)
 
-#return weight of the edge
+
 def cost(G, e):
+    #this function return weight of an edge e
     return G[e[0]][e[1]]["weight"]
 
-#merging/union edges that is duplicated
+
 def possible_edges(G, T):
+    #this function returns all possible edges after union all duplicated edges
     return [e for e in list(G.edges(V(T)))
             if e[0] not in V(T) or e[1] not in V(T)]
 
-
-#return lowest weight edge
 def min_cost_edge(G, T):
-    temp_edges = possible_edges(G, T) #return possible edges
+    #the function return the smallest edge of the graph. the function calls possible_edge in order to
+    #remove duplicated edge. Then the fuction will compare one edge to another using binary comparsion
+    #and store smallest value in min_edge. it will return min_edge after the loop exterminates
+    temp_edges = possible_edges(G, T) 
     #comparing one edge to another by weight to get the smalles edge weight
     min_edge = temp_edges[0]
     for e in temp_edges:
